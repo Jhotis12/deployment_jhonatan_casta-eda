ac82584249716bc062cf2bf3f4bdb4e2257c8771
jhonatan <137637410+Jhotis12@users.noreply.github.com>
2025-11-25 20:46:18 -0500

    componente contenedor de imagen

---
+++ src/components/ComponetImg.tsx
@@ -0,0 +1,12 @@
+export const ComponetImg = () => {
+  return (
+    <div className="w-full md:w-1/2 h-64 md:h-auto relative overflow-hidden rounded-r-2xl">
+      <img
+        className='w-full h-full object-cover'
+        src="https://media.istockphoto.com/id/1408934219/es/vector/retina-concepto-de-comer.jpg?s=612x612&w=0&k=20&c=b-cUx76tqUilhL_7mOGiBoq1OQ65cHbUg8R9AC4KROw="
+        alt="IMC-Mujer"
+      />
+      <div className="absolute inset-0 bg-indigo-900/20 mix-blend-multiply"></div>
+    </div>
+  )
+}
